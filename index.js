9258e93797b1bac00f0c85244385a82563740ded
Jaycee Porto <jaycee.porto@outlook.com>
2022-07-14 21:19:49 -0500

    Completed Exercise

---
+++ index.js
@@ -0,0 +1,22 @@
+const fs = require ("fs");
+const path = require('path');
+
+let fileNames = ["createFile", "readFile", "updateFile", "deleteFile"];
+let fileExt = "./js";
+let fileTemplate = `constfs = require("fs")`;
+
+fileNames.forEach((name) => {
+    fs.writeFile(
+        path.join(__dirname, `./${name}${fileExt}`), 
+        fileTemplate, 
+        (err) => {
+            if(err) {
+                console.error(err)
+                console.log(`Falied to write to ${name}${fileExt} x`);
+             } else {
+                 console.log(`Successfully tp write to ${name}${fileExt}`);
+             }
+        
+        
+        }
+})
---
+++ src/createFile.js
@@ -0,0 +1,10 @@
+const { wrtieFile } = require("fs");
+
+writeFile("./HelloWorld.txt", "Hello World!", (err) => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log("Successfully created file."); 
+    }
+});
+
